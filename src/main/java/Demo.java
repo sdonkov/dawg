ba9792ec077d9b4487017013b13893e94f1089c4
sdonkov <102171106+sdonkov@users.noreply.github.com>
2022-12-21 16:20:18 +0200

    add junit and two tests

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -3,23 +3,6 @@ import java.util.ArrayList;
 public class Demo {
 
     public static void main(String[] args) {
-    //TODO replace with a proper test
-        PrefixTree prefixTree = new PrefixTree();
-        prefixTree.add("axa");
-        prefixTree.add("abe");
-        prefixTree.add("tab");
-        prefixTree.add("table");
-        prefixTree.add("a");
-        prefixTree.add("ayasx");
-        prefixTree.add("ayasx");
 
-        System.out.println(prefixTree.contains("tabl")); // false
-        System.out.println(prefixTree.contains("table")); // true
-        System.out.println(prefixTree.contains("a")); // true
-        System.out.println(prefixTree.contains("ab")); // false
-        System.out.println(prefixTree.contains("abe")); // true
-        System.out.println(prefixTree.contains("tab")); //true
-        System.out.println(prefixTree.contains("axa")); // true
-        System.out.println(prefixTree.contains("ayasx"));//true
     }
 }
