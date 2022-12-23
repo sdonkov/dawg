c6a62cecb7f070da42a34653e465c0a3c6440132
sdonkov <102171106+sdonkov@users.noreply.github.com>
2022-12-23 14:24:27 +0200

    add package com.github.sdonkov

--- src/main/java/com/github/sdonkov/Demo.java
+++ src/main/java/com/github/sdonkov/Demo.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+package com.github.sdonkov;
 
 public class Demo {
 
--- src/main/java/com/github/sdonkov/PrefixTree.java
+++ src/main/java/com/github/sdonkov/PrefixTree.java
@@ -1,3 +1,5 @@
+package com.github.sdonkov;
+
 import java.util.ArrayList;
 import java.util.Optional;
 
