52792a7a3cad450fb7d856b8e8b248cfa3ef10d1
sdonkov <102171106+sdonkov@users.noreply.github.com>
2023-01-19 17:17:59 +0200

    add jmh dependencies, add benchmark tests, change root level to debug

--- src/test/java/com/github/sdonkov/PrefixTreeTest.java
+++ src/test/java/com/github/sdonkov/PrefixTreeTest.java
@@ -2,9 +2,13 @@ package com.github.sdonkov;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jol.info.GraphLayout;
 
 
@@ -16,32 +20,56 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(value = 1, warmups = 1)
+@State(Scope.Benchmark)
 public class PrefixTreeTest {
 
     private static List<String> words;
     private static PrefixTree prefixTree;
-
-    private static Set hashSet;
-    private static Set treeSet;
-
     private final static Logger LOGGER = LogManager.getLogger(PrefixTree.class);
+
     @BeforeAll
     static void setUp() throws IOException, URISyntaxException {
         prefixTree = new PrefixTree();
         words = Files.readAllLines(Paths.get(PrefixTreeTest.class.getResource("/dictionary.txt").toURI()));
         words.forEach(word -> prefixTree.add(word));
-        hashSet = new HashSet<>(words);
-        treeSet = new TreeSet<>(words);
     }
 
-    @AfterAll
-    static void tearDown() {
-        LOGGER.trace(GraphLayout.parseInstance(prefixTree).totalSize() + " tree");
+    @Test
+    public void launchBenchmark() throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(PrefixTreeTest.class.getSimpleName())
+                .build();
+        new Runner(opt).run();
+    }
+
+    @Benchmark
+    public void benchmarkPrefixTree() throws URISyntaxException, IOException {
+        prefixTree = new PrefixTree();
+        words = Files.readAllLines(Paths.get(PrefixTreeTest.class.getResource("/dictionary.txt").toURI()));
+        words.forEach(word -> prefixTree.add(word));
+        LOGGER.trace(GraphLayout.parseInstance(prefixTree).totalSize() + " prefixTree");
+    }
+
+    @Benchmark
+    public void benchmarkHashSet() throws URISyntaxException, IOException {
+        words = Files.readAllLines(Paths.get(PrefixTreeTest.class.getResource("/dictionary.txt").toURI()));
+        Set<String> hashSet = new HashSet<>(words);
         LOGGER.trace(GraphLayout.parseInstance(hashSet).totalSize() + " hashSet");
+
+    }
+
+
+    @Benchmark
+    public void benchmarkTreeSet() throws URISyntaxException, IOException {
+        words = Files.readAllLines(Paths.get(PrefixTreeTest.class.getResource("/dictionary.txt").toURI()));
+        Set<String> treeSet = new TreeSet<>(words);
         LOGGER.trace(GraphLayout.parseInstance(treeSet).totalSize() + " treeSet");
     }
 
